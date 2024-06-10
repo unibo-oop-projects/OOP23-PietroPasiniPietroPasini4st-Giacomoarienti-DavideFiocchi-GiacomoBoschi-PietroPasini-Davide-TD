55bd37ec2520f80833f3ddd5e7e9ce177219be6f
Danilo Pianini <danilo.pianini@unibo.it>
2024-06-10 12:42:55 +0200

    ci: add the OOP machinery

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     // Apply the java plugin to add support for Java
     java
@@ -13,7 +14,9 @@ plugins {
      */
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.danilopianini.gradle-java-qa") version "1.52.0"
-}
+
+    id("org.danilopianini.unibo-oop-gradle-plugin") version "1.0.13"}
+
 
 java {
     toolchain {
@@ -65,3 +68,5 @@ tasks.test {
         showStandardStreams = true
     }
 }
+
+ java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
